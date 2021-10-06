8cdc38188e7e977414ed9f4c38b73003d1b704ad
kashapovd <y.kashapovd@gmail.com>
2021-10-06 12:40:31 +0700

    added work1

--- ps-kill-lab1.sh
+++ ps-kill-lab1.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
+USER_TIME=$1
+LOG_FILE=$2
+
 usage() {
     echo "Usage: $0 <seconds> <file>"
     exit
 }
 
 pkill() {
-    local CLK_TCK=$(getconf CLK_TCK)
+	local CLK_TCK=$(getconf CLK_TCK)
 
-    while true; 
-    do
-        	
-	PIDS=$(ps -e -o pid h | head -n -2)
 
- 	echo "Found $(echo $PIDS | wc -w) processes"
-    
- 	for P in $PIDS
- 	do
-	    if [[ $(echo "$(cat /proc/$P/stat | awk '{print $14}') / $CLK_TCK" | bc) -gt $1 ]]; then
-		kill $P
-
-		$F <<< echo "$(date +%s) $P was killed\n" > $2
-	    fi
- 	done
-    done
+	while true;
+	do
+		PIDS=$(ps -e -o pid h | head -n -2)
+ 		echo "Found $(echo $PIDS | wc -w) processes"
+		for P in $PIDS
+		do
+			if [[ $(echo "$(cat /proc/$P/stat 2>&1 | awk '{print $14}' ) / $CLK_TCK" | bc) -gt $USER_TIME ]]; then
+				kill $P
+				echo "$(date +%s) $P was killed\\n" >> $LOG_FILE
+			fi
+		done
+	done
 }
 
-if [ -z $1 ]; then
+if [ -z $1 ] || [ -z $2 ]; then
     usage
 fi
 
