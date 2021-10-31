405c489f500d9ece21e6ff26d0223d385c75781f
kashapovd <y.kashapovd@gmail.com>
2021-10-31 21:26:24 +0700

    fixed work2

--- work2/grep.sh
+++ work2/grep.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-if [[ -z $1 ]]; then
-    journalctl | grep -P 'kernel:.*(?:Freeing).*(:?memory):.[1-9]\d{3,}'
+REGEXPR="kernel:.*(?:Freeing).*(:?memory):.[1-9]\d{2,}[1-9]"
+LOGFILE=$1
+
+if [[ -z $LOGFILE ]]; then
+    journalctl | grep -P $(echo $REGEXPR)
 else
-    if [[ -f ${0%/*}/$1 ]]; then
-	grep -P 'kernel:.*(?:Freeing).*(:?memory):.[1-9]\d{3,}' $1
+    if [[ -f $LOGFILE ]]; then
+	    grep -P $(echo $REGEXPR) $1
     else
 	echo "file $1 doesn't exist"
     fi
